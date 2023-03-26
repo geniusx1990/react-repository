2fbe38dd9eaef1e15f7f259b62cc542705c6ab3b
Matsvei Khmialnitski <geniusx1990@gmail.com>
2023-03-26 15:10:32 +0300

    fix: updated test coverage config file

--- react-ts-app/vite.config.ts
+++ react-ts-app/vite.config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
@@ -7,10 +6,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: ['./src/setupTests.ts'],
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      provider: 'c8',
+      reporter: 'text',
     },
+  },
 });
