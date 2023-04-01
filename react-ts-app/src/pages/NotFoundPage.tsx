a0466ebf9a340f82d682e5701d6afdf59c25bea8
Matsvei Khmialnitski <geniusx1990@gmail.com>
2023-04-01 11:57:39 +0300

    refactor: rewrite class NotFoundPage into function

--- react-ts-app/src/pages/NotFoundPage.tsx
+++ react-ts-app/src/pages/NotFoundPage.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class NotFoundPage extends React.Component {
-  render() {
-    return (
-      <>
-        <h1>We cannot found this page</h1>
-        <Link to="/">GO HOME</Link>
-      </>
-    );
-  }
+function NotFoundPage() {
+  return (
+    <>
+      <h1>We cannot found this page</h1>
+      <Link to="/">GO HOME</Link>
+    </>
+  );
 }
 export default NotFoundPage;
