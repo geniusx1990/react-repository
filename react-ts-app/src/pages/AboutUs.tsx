9e5ea86306c1b7fb99157641836766290c3ceee3
Matsvei Khmialnitski <geniusx1990@gmail.com>
2023-04-01 11:56:37 +0300

    refacotr: rewrite class aboutUs into function

--- react-ts-app/src/pages/AboutUs.tsx
+++ react-ts-app/src/pages/AboutUs.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Header from '../layout/header';
 
-class AboutUs extends React.Component {
-  render() {
-    return (
-      <>
-        <Header pageName="About Us" />
-        <h1>This is page ABOUT US</h1>
-      </>
-    );
-  }
+function AboutUs() {
+  return (
+    <>
+      <Header pageName="About Us" />
+      <h1>This is page ABOUT US</h1>
+    </>
+  );
 }
 export default AboutUs;
