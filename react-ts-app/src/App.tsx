797febc20ac1771f465872aa9064104daee68961
Matsvei Khmialnitski <geniusx1990@gmail.com>
2023-04-01 11:42:19 +0300

    refactor: rewrite in function

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import AboutUs from './pages/AboutUs';
 import Forms from './pages/Forms';
 import Home from './pages/Home';
 import NotFoundPage from './pages/NotFoundPage';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/forms" element={<Forms />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/forms" element={<Forms />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
