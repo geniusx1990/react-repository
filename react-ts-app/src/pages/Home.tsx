2dae00f835226250ee556ba17913ada5cb533274
Matsvei Khmialnitski <geniusx1990@gmail.com>
2023-04-01 11:55:00 +0300

    refactor: rewrite class homepage into function

--- react-ts-app/src/pages/Home.tsx
+++ react-ts-app/src/pages/Home.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
 import CardList from '../layout/CarldList';
 import Header from '../layout/header';
 import SearchBar from '../layout/searchBar';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <>
-        <Header pageName="Home" />
-        <SearchBar />
-        <CardList />
-      </>
-    );
-  }
+function Home() {
+  return (
+    <>
+      <Header pageName="Home" />
+      <SearchBar />
+      <CardList />
+    </>
+  );
 }
 export default Home;
